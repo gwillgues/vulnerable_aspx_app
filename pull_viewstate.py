a6dc5f8c1fb6a8839478bd24891b9c514d5c9683
gwillgues <96144967+gwillgues@users.noreply.github.com>
2026-05-27 17:37:41 -0400

    Add generator extraction to pull_viewstate.py

--- pull_viewstate.py
+++ pull_viewstate.py
@@ -6,16 +6,20 @@ import sys
 
 target = sys.argv[1]
 #pattern = 'name=\"__VIEWSTATE\" value=\"(?P<viewstate_b64>[a-zA-Z0-9+\=\_\/])\"'
-pattern = r'id="__VIEWSTATE"\svalue="(?P<viewstate_b64>[a-zA-Z0-9+=_/]{3,128})"'
-
+viewstate_pattern = r'id="__VIEWSTATE"\svalue="(?P<viewstate_b64>[a-zA-Z0-9+=_/]{3,128})"'
+generator_pattern = r'id="__VIEWSTATEGENERATOR"\svalue="(?P<generator_hex>[a-fA-F0-9]{8})"'
 
 resp = requests.get(f'http://{target}:80/test.aspx')
 #Regex match to search for base64 encoded VIEWSTATE string
-match = re.search(pattern, resp.text)
-
-if match:
-    viewstate_dict = match.groupdict()
+viewstate_match = re.search(viewstate_pattern, resp.text)
+generator_match = re.search(generator_pattern, resp.text)
+if viewstate_match and generator_match:
+    viewstate_dict = viewstate_match.groupdict()
+    generator_dict = generator_match.groupdict()
     if viewstate_dict['viewstate_b64']:
         viewstate_data = viewstate_dict['viewstate_b64']
-    print(f"ViewState: {viewstate_data}")
+    if generator_dict['generator_hex']:
+        generator_data = generator_dict['generator_hex']
+    
+    print(f"ViewState: {viewstate_data} , Generator: {generator_data}")
 
